5ac83c295a2608d4ee2d27d9a742fef79f2f5b26
gerudaeta <gerudaeta@gmail.com>
2019-06-05 13:43:41 -0300

    add ui usuarios lista

--- src/app/views/usuarios-management/usuarios-management-list/usuarios-management-list.component.ts
+++ src/app/views/usuarios-management/usuarios-management-list/usuarios-management-list.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MatSnackBar } from '@angular/material';
+import { Subscription } from 'rxjs';
+
+// Servicios
+import { AppConfirmService} from '../../../shared/services/app-confirm/app-confirm.service';
+import { AppLoaderService} from '../../../shared/services/app-loader/app-loader.service';
+import { UsuariosManagementService } from '../usuarios-management.service';
+
+// Animacion
+import { egretAnimations } from '../../../shared/animations/egret-animations';
+
+// Componente
+import { UsuariosManagementFormComponent } from '../usuarios-management-form/usuarios-management-form.component';
 
 @Component({
-  selector: 'app-usuarios-management-list',
-  templateUrl: './usuarios-management-list.component.html',
-  styleUrls: ['./usuarios-management-list.component.scss']
+    selector: 'app-usuarios-management-list',
+    templateUrl: './usuarios-management-list.component.html',
+    styleUrls: ['./usuarios-management-list.component.scss'],
+    animations: egretAnimations
 })
 export class UsuariosManagementListComponent implements OnInit {
 
-  constructor() { }
+    items: any[];
+    getItemSub: Subscription;
 
-  ngOnInit() {
-  }
+    constructor(private dialog: MatDialog,
+                private snack: MatSnackBar,
+                private confirmService: AppConfirmService,
+                private loader: AppLoaderService,
+                private usuariosService: UsuariosManagementService) {
+    }
+
+    ngOnInit() {
+    }
+
+    openPopUp(data: any = {}, isNew?) {
+        let title = isNew ? 'Agregar Usuario' : 'Modificar Usuario';
+        let dialogRef: MatDialogRef<any> = this.dialog.open(UsuariosManagementFormComponent, {
+            width: '720px',
+            disableClose: true,
+            data: { title: title, payload: data }
+        });
+        dialogRef.afterClosed()
+            .subscribe(res => {
+                if (!res) {
+                    // If user press cancel
+                    return;
+                }
+                this.loader.open();
+                if (isNew) {
+                    // this.vehiculoService.addItem(res)
+                    //     .subscribe( data => {
+                    //         this.items = data;
+                    //         this.loader.close();
+                    //         this.snack.open('Member Added!', 'OK', { duration: 4000 });
+                    //     });
+                } else {
+                    // this.vehiculoService.updateItem(data._id, res)
+                    //     .subscribe( data => {
+                    //         this.items = data;
+                    //         this.loader.close();
+                    //         this.snack.open('Member Updated!', 'OK', { duration: 4000 });
+                    //     });
+                }
+            });
+    }
 
 }
