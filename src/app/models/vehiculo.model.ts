126bb5905910f147b87ace6f3db5ce817934cb2e
gerudaeta <gerudaeta@gmail.com>
2019-06-11 21:47:30 -0300

    add clases

---
+++ src/app/models/vehiculo.model.ts
@@ -0,0 +1,11 @@
+export class Vehiculo {
+    id: number;
+    tipoEquipo: number;
+    subTipoEquipo: number;
+    dominio: string;
+    año: Date;
+    marca: number;
+    modelo: number;
+    codigo: string;
+    codigoQR: string;
+}
