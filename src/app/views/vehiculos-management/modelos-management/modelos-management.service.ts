a97a0223ad2307d1e07a12771906016d2f18424e
gerudaeta <gerudaeta@gmail.com>
2019-06-19 02:04:56 -0300

    fix servicio modelos

--- src/app/views/vehiculos-management/modelos-management/modelos-management.service.ts
+++ src/app/views/vehiculos-management/modelos-management/modelos-management.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
-import { RequestService } from '../../../core/services';
 import { Observable } from 'rxjs';
+
+// Servicio
+import { RequestService } from '../../../core/services';
+
+// Modelo - Clase
+import { Modelo } from '../../../models/modelo.model';
 
 @Injectable({
     providedIn: 'root'
@@ -20,11 +25,11 @@ export class ModelosManagementService {
         return this.requestService.get(`${this.modelosURL}/` + id);
     }
 
-    public add(modelo: any): Observable<any> {
+    public add(modelo: Modelo): Observable<Modelo> {
         return this.requestService.post(`${this.modelosURL}`, modelo);
     }
 
-    public update(id: any, modelo: any): Observable<any> {
+    public update(id: any, modelo: Modelo): Observable<Modelo> {
         return this.requestService.put(`${this.modelosURL}/${id}`, modelo);
     }
 
